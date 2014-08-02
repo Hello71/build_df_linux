558c52d384627d3c1c4889478d964c0885106567
Alex Xu <alex_y_xu@yahoo.ca>
2014-08-02 15:40:27 -0400

    get_df.sh: Check for build system before fetching

--- get_df.sh
+++ get_df.sh
@@ -3,6 +3,15 @@ DF_VER=40_05
 
 set -e
 
+if [ -x ./build.sh ]; then
+    BUILD="./build.sh"
+elif [ -f Makefile ]; then
+    BUILD="make $MAKEFLAGS"
+else
+    echo "You must select a build system, see README for details." >&2
+    exit 1
+fi
+
 unpack() {
     tar -xj --exclude="df" --exclude="libs/*.so*" --exclude="README.linux" --exclude="sdl" --strip-components=1 "$@"
 }
@@ -13,7 +22,4 @@ else
     curl http://www.bay12games.com/dwarves/df_${DF_VER}_linux.tar.bz2 | unpack
 fi
 
-[ -x ./build.sh ] && exec ./build.sh
-[ -f Makefile ] && exec make "$MAKEFLAGS"
-echo "You must select a build system, see README for details." >&2
-exit 1
+exec $BUILD
