55b3dbfa77162b805e0bd8d27c61df38e701fa62
Alex Xu <alex_y_xu@yahoo.ca>
2015-04-17 12:35:53 -0400

    get_df.sh: add version selection, fix minor bugs.

--- get_df.sh
+++ get_df.sh
@@ -16,16 +16,31 @@ unpack() {
     tar -xj --exclude="df" --exclude="libs/*.so*" --exclude="README.linux" --exclude="sdl" --strip-components=1 "$@"
 }
 
-if [ -n "$1" ]; then
+if [ -f "$1" ]; then
     f=${1##*/}
-    [[ $f == df_??_??_linux.tar.bz2 ]] || echo "WARNING: $f does not look like an official DF tarball." >&2
-    echo "Unpacking Dwarf Fortress from ${1}..." >&2
+    case "$f" in
+        df_??_??_linux.tar.bz2) ;;
+        *) printf "WARNING: %s does not look like an official DF tarball, continuing anyways.\n" "$f" >&2
+    esac
+    printf "Unpacking Dwarf Fortress from %s...\n" "$1" >&2
     unpack -f "$1"
 else
-    echo "Downloading and unpacking Dwarf Fortress..." >&2
-    DF="$(curl http://www.bay12games.com/dwarves/ 2>/dev/null | grep -om1 'df_[0-9][0-9]_[0-9][0-9]_linux.tar.bz2')"
-    curl http://www.bay12games.com/dwarves/"$DF_VER" | unpack
+    maj="0."
+    case "$1" in
+    *.*.*)
+        min="${1#${maj}}"
+        DF="df_${min//./_}_linux.tar.bz2"
+        ver="$1"
+        ;;
+    *)
+        printf "No argument provided, finding latest DF version...\n" >&2
+        DF="$(curl http://www.bay12games.com/dwarves/ 2>/dev/null | grep -Eom1 'df_[0-9]{2}_[0-9]{2}_linux\.tar\.bz2')"
+        tmp="${df#df_}"
+        tmp="${tmp%%_linux*}"
+        ver="${maj}${tmp//_/.}"
+    esac
+    printf "Downloading and unpacking DF %s...\n" "$ver" >&2
+    curl http://www.bay12games.com/dwarves/"$DF" | unpack
 fi
 
-echo "Building Dwarf Fortress..." >&2
-exec $BUILD
+printf "Done. You can now run \`%s' to build DF." "$BUILD"
